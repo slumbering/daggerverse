3f5aba24ae5d0c4818906dc7d5e13cd544e05abb
jffarge <jf@dagger.io>
2023-10-03 14:11:33 +0100

    Use module to build a nextJs app
    Signed-off-by: jffarge <jf@dagger.io>

--- nextjs-build/main.go
+++ nextjs-build/main.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"context"
+	"fmt"
 )
 
 type NextjsBuild struct {}
 
-func (m *NextjsBuild) MyFunction(ctx context.Context, stringArg string) (*Container, error) {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg}).Sync(ctx)
+const (
+	// https://hub.docker.com/_/node
+	nodeJSVersion = "18"
+
+	// https://hub.docker.com/_/alpine
+	alpineVersion = "3.18"
+)
+
+func (d *Directory) BuildNextJS(ctx context.Context) (*Directory, error) {
+	node := dag.Container(ContainerOpts{Platform: Platform("linux/amd64")}).
+		From(fmt.Sprintf("node:%s", nodeJSVersion))
+	yarnCachePath := fmt.Sprintf("/usr/local/share/.cache/yarn/%s", nodeJSVersion)
+	yarnCacheVolume := fmt.Sprintf("dagger-io-yarn-%s", nodeJSVersion)
+
+	build, err := node.Pipeline("build").
+		WithDirectory("/app", d).
+		WithMountedCache(yarnCachePath, dag.CacheVolume(yarnCacheVolume)).
+		WithEnvVariable("YARN_CACHE_FOLDER", yarnCachePath).
+		WithWorkdir("/app").
+		WithExec([]string{"yarn", "install"}).
+		WithExec([]string{"yarn", "export"}).
+		Sync(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return build.Directory("out/"), nil
 }
