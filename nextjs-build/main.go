19a9f5dac1dfb2c0b080dfc8b31326cabcf30363
jffarge <jf@dagger.io>
2023-10-10 17:12:27 +0200

    Remove nginx config
    Signed-off-by: jffarge <jf@dagger.io>

--- nextjs-build/main.go
+++ nextjs-build/main.go
@@ -8,7 +8,7 @@ type NextjsBuild struct{}
 
 const (
 	// https://hub.docker.com/_/node
-	nodeJSVersion = "18"
+	nodeJSVersion = "slim"
 
 	// https://hub.docker.com/_/nginx
 	nginxVersion = "1.25"
@@ -25,35 +25,20 @@ func (d *Directory) NextJSBuild() *Container {
 func (m *NextjsBuild) Build(source *Directory) *Container {
 	node := dag.Container(ContainerOpts{Platform: Platform("linux/amd64")}).
 		From(fmt.Sprintf("node:%s", nodeJSVersion))
-	yarnCachePath := fmt.Sprintf("/usr/local/share/.cache/yarn/%s", nodeJSVersion)
+	yarnCachePath := "/Users/slumbering/Library/Caches/Yarn/v6"
 	yarnCacheVolume := fmt.Sprintf("dagger-io-yarn-%s", nodeJSVersion)
 
-	build := node.Pipeline("build").
-		WithDirectory("/app", source).
+	return node.Pipeline("build").
+		WithDirectory("/app", source, ContainerWithDirectoryOpts{
+			Exclude: []string{
+				"node_modules",
+				".next",
+				"out",
+			},
+		}).
 		WithMountedCache(yarnCachePath, dag.CacheVolume(yarnCacheVolume)).
 		WithEnvVariable("YARN_CACHE_FOLDER", yarnCachePath).
 		WithWorkdir("/app").
 		WithExec([]string{"yarn", "install"}).
 		WithExec([]string{"yarn", "export"})
-
-	return dag.Container(ContainerOpts{Platform: Platform("linux/amd64")}).
-		From(fmt.Sprintf("nginx:%s", nginxVersion)).
-		WithDirectory("/usr/share/nginx/html", build.Directory("/app/out")).
-		WithNewFile("/etc/nginx/conf.d/default.conf", ContainerWithNewFileOpts{
-			Contents: `
-				server {
-					listen       80;
-					listen  [::]:80;
-					server_name  localhost;
-
-					location / {
-						root   /usr/share/nginx/html;
-						try_files $uri $uri/index.html $uri.html =404;
-					}
-
-					error_page  404 	/404.html;
-
-					error_page  500 502 503 504 	/50x.html;
-				}
-			`})
 }
